6beea140e1783808dae1e9060b913fbd6209da82
shan.du <shan.du@nurun.com>
2013-10-09 19:38:27 -0400

    updated create card page

--- app/models/request/AddCardRequest.scala
+++ app/models/request/AddCardRequest.scala
@@ -5,7 +5,7 @@ import org.joda.time.DateTime
 import play.api.libs.json.Json
 
 case class AddCardRequest( recipients: Seq[String],
-                           coSenders: Seq[String],
+                           senders: Seq[String],
                            message: String) {
 }
 
