e13e6e3d800867b83ac5436114d1c72caeab228c
Dana Harrington <dana.harrington@nurun.com>
2013-10-09 17:33:50 -0400

    Working on auth, currently pass username as querystring param to addCard, updated Postman defs

--- app/controllers/CardController.scala
+++ app/controllers/CardController.scala
@@ -4,8 +4,18 @@ import play.api.mvc._
 import services._
 import play.api.libs.json._
 import models.request.AddCardRequest
+import play.mvc.Security.Authenticated
 
-object CardController extends Controller {
+/*trait Auth extends Controller {
+  def authenticated[A](f: => String => Action[A]): Action[A] = Action { request: Request[AnyContent] =>
+    request.cookies.get("username") match {
+      case Some(username) => f(username)(request)
+      case None => NotFound()
+    }
+  }
+}*/
+
+object CardController extends Controller { //with Auth {
 
   def getCard(id: Int) = Action {
     CardService.getCard(id) match {
@@ -14,15 +24,17 @@ object CardController extends Controller {
     }
   }
 
-  def addCard() = Action(parse.json) { request =>
-    val username = ""
-    request.body.asOpt[AddCardRequest] match {
-      case Some(card) =>
-        CardService.addCard(username, card)
-        Ok("ok")
-      case None =>
-        BadRequest
-    }
+  def addCard() = //authenticated { username =>
+    Action(parse.json) { request =>
+      request.body.asOpt[AddCardRequest] match {
+        case Some(card) =>
+          val username = request.queryString.get("username").flatMap(_.headOption).getOrElse("")
+          CardService.addCard(username, card)
+          Ok("ok")
+        case None =>
+          BadRequest
+      }
+    //}
   }
 
   def getCards = Action { request =>
